697a7d10f0e00a2d5aafc6e681de6084221016c0
Pedro Sader Azevedo <pedro.saderazevedo@lnbr.cnpem.br>
2024-07-29 13:33:04 -0300

    add a UI on top of the model
    The code for the UI that follows the model is heavily based on
    https://github.com/gftruj/webzamples/blob/409b9fa17762e106a932367d70f5e092000a1e72/arjs/tracked-ui/tracked-ui.js

---
+++ tracked-ui.js
@@ -0,0 +1,50 @@
+AFRAME.registerComponent("tracked-ui", {
+	schema: {
+		element: {default: ""},
+		autoShow: {default: true},
+		offset: {type: 'vec2', default: {x: 0, y: 0}}
+	},
+    init: function() {
+        // THREE.Vector3 helper. 
+        this.vector = new THREE.Vector3();
+    },
+    update: function() {
+        // references
+        this.element = document.querySelector(this.data.element)
+    },
+    tick: function() {
+        if (!this.element) return;
+
+        // show the UI only if the marker is visible
+        if (this.data.autoShow) {
+            if (this.el.object3D.visible &&
+                (this.el.object3D.rotation._y >= -1.40 && this.el.object3D.rotation._y <= -0.05 && this.el.object3D.rotation._x >= 0.00 && this.el.object3D.rotation._x <= 0.90)
+            ) {
+                this.element.style.display = "block"
+            } else {
+                this.element.style.display = "none"
+            }
+        }
+
+        // project the current marker position
+        this.vector.copy(this.el.object3D.position).project(this.el.sceneEl.camera);
+
+        // the vector is now normalized, so we need to adjust it to the dimensions
+        this.vector.x = ( this.vector.x + 1) * window.innerWidth / 2;
+        this.vector.y = - ( this.vector.y - 1) * window.innerHeight / 2;
+
+	// get distance from camera
+	let camera = document.querySelector('[camera]');
+	let cameraPosition = camera.object3D.position;
+        let markerPosition = this.el.object3D.position;
+        let distance = cameraPosition.distanceTo(markerPosition)
+
+        // apply the vector to the style
+        this.element.style.left = (this.vector.x + this.data.offset.x / distance) + 'px';
+        this.element.style.top = (this.vector.y + this.data.offset.y / distance) + 'px';
+
+	// adjust image size
+	let image = document.getElementById('dockingicon');
+	image.style.width = (1650 / distance) + 'px';
+	}
+})
